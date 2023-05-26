b878ea2fdff760f2b6287e10ca3e82e27e2c75dc
Minminjamin <0429el@gmail.com>
2023-05-26 14:40:37 +0900

    style

--- src/components/SideBar/UserProfileCard.tsx
+++ src/components/SideBar/UserProfileCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const UserProfileCard = () => {
-  const userName: string = "";
-  const userId: string = "";
+  const userName: string = "user의 이름이 들어오는 칸";
+  const userId: string = "user의 아이디가 들어오는 칸";
 
   return (
     <div>
