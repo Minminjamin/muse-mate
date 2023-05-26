c4359ac633073437161f5a0dcf3544aed0ffb6f5
Minminjamin <0429el@gmail.com>
2023-05-26 10:07:52 +0900

    feature : SideMenu 컴포넌트

---
+++ src/components/SideBar/SideMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideMenu = () => {
+  return (
+    <ul>
+      <li>대충 메뉴 1</li>
+      <li>대충 메뉴 2</li>
+      <li>대충 메뉴 3</li>
+    </ul>
+  );
+};
+
+export default SideMenu;
