c5c48409d12183a4537f7ac0a834303a0b79a016
Minminjamin <0429el@gmail.com>
2023-05-30 11:45:05 +0900

    feature : Home 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SideBar from "./routes/SideBar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <SideBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
