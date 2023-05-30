0b215d8e3b5de2cd38266eab4564a83726370f97
Minminjamin <0429el@gmail.com>
2023-05-30 10:07:29 +0900

    refact : map으로 변경

--- src/components/SideBar/SideMenuList.tsx
+++ src/components/SideBar/SideMenuList.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 const SideMenuList = () => {
+  const menuList = [
+    {
+      name: "검색",
+      url: "search",
+    },
+    {
+      name: "내 플레이리스트",
+      url: "playList",
+    },
+    { name: "친구", url: "friend" },
+  ];
+
   return (
-    <ul>
-      <li>대충 메뉴 1</li>
-      <li>대충 메뉴 2</li>
-      <li>대충 메뉴 3</li>
-    </ul>
+    <>
+      <ul>
+        {menuList.map((item) => (
+          <li key={item.name}>
+            <span>{item.name}</span>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
