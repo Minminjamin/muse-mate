570f476afd0ffbac34045f3dcbfd3dc970438788
Minminjamin <0429el@gmail.com>
2023-05-30 11:48:38 +0900

    feature : RecommendedPlaylists 컴포넌트 추가

---
+++ src/components/Home/RecommendedPlaylists.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const RecommendedPlaylists = () => {
+  return (
+    <div>
+      <span>추천 플레이 리스트</span>
+
+      <div>
+        <div>추천 플리1</div>
+        <div>추천 플리2</div>
+        <div>추천 플리3</div>
+        <div>추천 플리4</div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedPlaylists;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Home/Banner";
+import RecommendedPlaylists from "../components/Home/RecommendedPlaylists";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <RecommendedPlaylists />
     </div>
   );
 };
